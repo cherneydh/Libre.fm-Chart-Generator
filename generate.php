d71056fad71d8970f54e4d365b5a2d1f6c33094b
Daniel Cherney <31091775+cherneydh@users.noreply.github.com>
2018-02-07 10:05:25 -0800

    Grabs XML from Libre.fm

--- generate.php
+++ generate.php
@@ -1,4 +1,7 @@
 <?php
 # I have no idea what I am doing
-echo $_POST["user"];
+$user = $_POST["user"];
+$url = "http://libre.fm/2.0/?method=user.getrecenttracks&user=" . $user . "&page=1&limit=100";
+$xml = simplexml_load_file($url);
+print_r($xml);
 ?>
