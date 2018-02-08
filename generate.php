471d518b15c99b8b2992d9674b91c79bb7125cb9
Daniel <cherneydh>
2018-02-07 21:14:13 -0800

    Added db functionality

--- generate.php
+++ generate.php
@@ -1,7 +1,53 @@
 <?php
+include('/home/daniel/Desktop/MySQL/signin.php');
+
 # I have no idea what I am doing
+$count = 9;
+
+#Create Temporary MySQL Table
+$entry = "DROP TEMPORARY TABLE IF EXISTS albums";
+mysqli_query ( $conn, $entry ) or die(mysqli_error($conn));
+
+$entry = "CREATE TEMPORARY TABLE albums
+		(
+			artist VARCHAR(100) NOT NULL,
+			album VARCHAR(100) NOT NULL
+		) ";
+mysqli_query ( $conn, $entry ) or die(mysqli_error($conn));
+
+#Set Page Number
+$page = 1;
+
+#Get username for user
 $user = $_POST["user"];
-$url = "http://libre.fm/2.0/?method=user.getrecenttracks&user=" . $user . "&page=1&limit=100";
+
+#Get XML for past 100 results
+$url = "http://libre.fm/2.0/?method=user.getrecenttracks&user=" . $user . "&page=" . $page . "&limit=100";
 $xml = simplexml_load_file($url);
-print_r($xml);
+if (!$xml) {
+	echo '<h2>Error trying to retrieve that user\'s data!</h2><br>';
+	exit;
+}
+
+echo "<h1>" . $user . "'s Top 9 Albums for the Past Week </h1>";
+
+$artist = $xml->xpath('/lfm/recenttracks/track/artist');
+$album = $xml->xpath('/lfm/recenttracks/track/album');
+
+while(list( , $node1) = each($artist)) {
+	while(list( , $node2) = each($album)) {
+		$entry = "INSERT INTO albums VALUES (\"" . $node1 . "\", \"" . $node2 . "\")";
+		mysqli_query( $conn, $entry) or die(mysqli_error($conn)); 
+	}
+}
+
+$sql = mysqli_query( $conn, "SELECT album,COUNT(*) as count FROM albums GROUP BY album ORDER BY count DESC LIMIT " . $count) or die(mysqli_error($conn));
+
+while($row = mysqli_fetch_assoc($sql)){
+	foreach($row as $key => $val){
+		echo $key . ": " . $val . "<br>";
+	}
+}
+
+$conn->close();
 ?>
